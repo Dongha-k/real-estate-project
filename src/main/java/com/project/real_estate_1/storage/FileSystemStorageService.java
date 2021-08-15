bcfbd501626ad1d5d06fd96a0e0ab79555d17f0d
Dongha-k <75309927+Dongha-k@users.noreply.github.com>
2021-08-15 17:28:51 +0900

    테스트 구성

--- src/main/java/com/project/real_estate_1/storage/FileSystemStorageService.java
+++ src/main/java/com/project/real_estate_1/storage/FileSystemStorageService.java
@@ -29,7 +29,7 @@ public class FileSystemStorageService implements StorageService{
 
     @Override
     public String store(MultipartFile file){
-        if(file == null) System.out.println("사진이 null 임");
+        System.out.println(rootLocation.toString());
         try{
             if(file.isEmpty()){
                 throw new StorageException("Failed to store empty file " + file.getOriginalFilename());
