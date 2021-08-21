16f7820f43fc338807f9d3e5c8f7b3b153bfc568
Dongha-k <75309927+Dongha-k@users.noreply.github.com>
2021-08-21 22:39:43 +0900

    contract 수정

--- src/main/java/com/project/real_estate_1/controller/deal/ContractController.java
+++ src/main/java/com/project/real_estate_1/controller/deal/ContractController.java
@@ -121,6 +121,7 @@ public class ContractController {
             httpHeaders.add("code", "98");
             return new ResponseEntity<>(null, httpHeaders, HttpStatus.OK);
         } catch (Exception e){
+            System.out.println(e.getMessage());
             httpHeaders.add("code", "99");
             return new ResponseEntity<>(null, httpHeaders, HttpStatus.OK);
         }
